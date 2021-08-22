c6ed820b919e06a08af952a7e58d190c1d56e657
Danita Forbes <danitacodes@gmail.com>
2021-08-21 22:12:11 -0700

    still trying to deploy

--- server.js
+++ server.js
@@ -208,8 +208,8 @@ app.use(errorHandler);
 if (process.env.NODE_ENV === "production") {
   const path = require("path");
 
-  app.use(express.static(path.resolve(__dirname, "client", "build")));
   app.get("/", (req, res) => {
+    app.use(express.static(path.resolve(__dirname, "client", "build")));
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
